5b4937d70993591916a18b33598a06fc1bf01b03
dgarciah98 <dieg0farrobaterra@gmail.com>
2021-12-23 21:58:46 +0100

    page set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bart-ip-page';
+export class AppComponent implements OnInit {
+	title: string;	
+	ipAddress: string;
+	music = document.getElementById("music");
+	
+	constructor(private http:HttpClient) {
+		this.ipAddress = "";
+		this.title = "BartIP";
+		sessionStorage.clear();		
+	}
+
+	ngOnInit() {
+		this.getIPAddress();
+	}
+
+	getIPAddress() {
+		this.http.get("https://ifconfig.me", {responseType: "text"}).subscribe(ip => this.ipAddress = ip);					
+	}
+
 }
