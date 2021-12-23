2d49e96219fd6bb98142ae1c3a57a09f505f595c
dgarciah98 <dieg0farrobaterra@gmail.com>
2021-12-23 23:16:58 +0100

    i deleted bootstrap thinking i didnt need it and im dumb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
 	declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
 		HttpClientModule,
 		BrowserModule,
 		AppRoutingModule,
+		NgbModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
